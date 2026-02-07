a8d757e6ec3e3ce9d3a73237ed27b0e0795e3c5d
everettjf <everettjf@live.com>
2026-02-07 01:01:06 -0800

    logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -128,7 +128,7 @@ const config = {
         title: 'MicroClaw',
         logo: {
           alt: 'MicroClaw Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
